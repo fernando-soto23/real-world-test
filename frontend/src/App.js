8e7aa256484f8cfe17c9e746b1da948b24ee1fd7
Fernando Soto <fernando.sotojacome@thoughtworks.com>
2025-03-27 10:27:17 -0500

    feat: real-world scaffolding

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import { Home } from "./pages/Home/Home";
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <h1>hola!!como estas It's finished!!!</h1>
+    </div>
+  );
+}
+
+export default App;
